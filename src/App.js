b9ee12db06464000d5cca5b16573de0116f2875b
Priyom <priyom1499@gmail.com>
2024-06-24 21:39:02 +0530

    Added Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Body from "./Components/Body";
 import Footer from "./Components/Footer";
-import TermsAndConditions from "./Components/TermsAndConditions";
-import PrivacyPolicy from "./Components/PrivacyPolicy";
+
+// Lazy loading components
+const TermsAndConditions = lazy(() =>
+  import("./Components/TermsAndConditions")
+);
+const PrivacyPolicy = lazy(() => import("./Components/PrivacyPolicy"));
 
 function App() {
   return (
     <Router>
       {/* <Navbar /> */}
-      <Routes>
-        <Route path="/" element={<Body />} />
-        <Route path="/terms&condition" element={<TermsAndConditions />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-      </Routes>
-
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Body />} />
+          <Route path="/terms&condition" element={<TermsAndConditions />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </Router>
   );
